9c32867cbe4e3098bbd234893dba2529b1239b3b
Sam Whittaker-Smith <samwhittakersmith@sams-macbook-pro-2.local>
2019-02-22 08:55:19 +1100

    Convert to MVC design pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,15 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Sam Whittaker-Smith on 13/2/19.
+//  Copyright © 2019 Sam Whittaker-Smith. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    var title: String = ""
+    var done: Bool = false
+}
